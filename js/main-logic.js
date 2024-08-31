f52e1a2ae9d10d3fb9827cfebc4036acebc45402
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2024-08-31 15:09:18 +0200

    Improved visuals

--- js/main-logic.js
+++ js/main-logic.js
@@ -40,21 +40,28 @@ function returnStatus(a, b) {
 function goTo(destinationFloor, currentFloor, nearestElevator, finalDestination) {
 	let floorCounter = 0;
 	const totalFloors = Math.abs(destinationFloor - currentFloor);
+	nearestElevator.domElement.innerHTML = `<span class="arrow"></span><span class='destination-floor'>${destinationFloor}</span>`;
+	if (nearestElevator.status == STATUS.MOVING_UP) nearestElevator.domElement.querySelector('.arrow').innerHTML = '&#8679;';
+	if (nearestElevator.status == STATUS.MOVING_DOWN) nearestElevator.domElement.querySelector('.arrow').innerHTML = '&#8681;';
 	let timer = setInterval(() => {
 		if (floorCounter >= totalFloors) {
 			nearestElevator.status = finalDestination !== null && finalDestination !== undefined ? STATUS.READY : STATUS.IDLE;
 			nearestElevator.currentFloor = destinationFloor;
+			if (nearestElevator.status == STATUS.MOVING_UP) nearestElevator.domElement.querySelector('.arrow').innerHTML = '&#8679;';
+			if (nearestElevator.status == STATUS.MOVING_DOWN) nearestElevator.domElement.querySelector('.arrow').innerHTML = '&#8681;';
 			if (nearestElevator.status === STATUS.READY) {
 				console.log(`The elevator NUMBER ${nearestElevator.id} is READY. Moving to final destination.`);
 				nearestElevator.domElement.classList.add('pause');
 				setTimeout(() => {
 					pickUpPassanger(nearestElevator);
 					nearestElevator.domElement.classList.remove('pause');
+
 					goTo(finalDestination, destinationFloor, nearestElevator, null);
 				}, 800);
 			} else {
 				nearestElevator.domElement.classList.remove('active');
 				nearestElevator.domElement.classList.remove('active-random');
+				nearestElevator.domElement.innerHTML = ``;
 				console.log(`The elevator NUMBER ${nearestElevator.id} is now idle again.`, nearestElevator);
 			}
 			clearInterval(timer);
@@ -65,10 +72,16 @@ function goTo(destinationFloor, currentFloor, nearestElevator, finalDestination)
 
 			if (finalDestination == null && finalDestination == undefined) pickUpPassanger(nearestElevator);
 			if (nearestElevator.status == STATUS.MOVING_UP) {
+				nearestElevator.domElement.querySelector('.arrow').classList.remove('moving-down');
+				nearestElevator.domElement.querySelector('.arrow').classList.add('moving-up');
+				nearestElevator.domElement.querySelector('.arrow').innerHTML = '&#8679;';
 				nearestElevator.coordinates.floor.y = nearestElevator.coordinates.floor.y - 1 - 50;
 				nearestElevator.domElement.style.top = `${nearestElevator.coordinates.floor.y}px`;
 			}
 			if (nearestElevator.status == STATUS.MOVING_DOWN) {
+				nearestElevator.domElement.querySelector('.arrow').classList.remove('moving-up');
+				nearestElevator.domElement.querySelector('.arrow').classList.add('moving-down');
+				nearestElevator.domElement.querySelector('.arrow').innerHTML = '&#8681;';
 				nearestElevator.coordinates.floor.y = nearestElevator.coordinates.floor.y + 1 + 50;
 				nearestElevator.domElement.style.top = `${nearestElevator.coordinates.floor.y}px`;
 			}
